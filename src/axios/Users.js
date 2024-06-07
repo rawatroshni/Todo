2b921aab7a7dcbaa1fd31fa26f87d2155e4d03e9
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-06-07 12:27:21 +0530

    added display and search functionality with axios

--- src/axios/Users.js
+++ src/axios/Users.js
@@ -1,26 +1,47 @@
 import axios from 'axios';
-import React from 'react'
-const API_URL = "https://jsonplaceholder.typicode.com/user"
+import React, { useEffect, useState } from 'react'
+const API_URL = "https://jsonplaceholder.typicode.com/users"
 const Users = () => {
-
+const [users,setUsers]=useState([])
+const [search , setSearch] = useState('')
 
     const fetchUsers = () => {
-
-
-
         axios.get(API_URL).then((res) => {
             console.log(res);
+            setUsers(res.data)
         }).catch((err) => {
             console.log(err);
         })
     }
-
-
+useEffect(()=>{
     fetchUsers()
+},[])
+
+      
 
 
     return (
+        <>
+        <div>
+
         <div>Users</div>
+        <input type='search' onChange={(e)=>setSearch(e.target.value.toLowerCase())} className='form-control w-25' placeholder='Search Users'/>
+        </div>
+        {
+            users.filter((item)=>item.name.toLowerCase().includes(search)).map((item)=>{
+                return <div>{item.name}</div>
+            })
+        }
+         {
+            users.filter((item)=>item.name.toLowerCase().includes(search)).length === 0 && (
+                <div>
+                    {
+                        `User with this name ${search} does't exist `
+                    }
+                </div>
+            )
+         }
+        </>
     )
 }
 
